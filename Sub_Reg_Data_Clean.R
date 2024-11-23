7a4b167b96fb575290ddc06383c036c91761f013
Conall Templeman <ctt21@ic.ac.uk>
2024-11-23 14:35:46 +0000

    Extract area codes

--- Sub_Reg_Data_Clean.R
+++ Sub_Reg_Data_Clean.R
@@ -28,8 +28,12 @@ LIPEE_conv <- read.csv("Conversion Pattern LIHC to LILEE.csv", header = TRUE)
 
 Sub_Reg_Files <- list.files("Sub-Regional Data", full.names = TRUE)
 
-# Identify area codes
-Area_codes = read_xlsx(Sub_Reg_Files[length(Sub_Reg_Files)], sheet = "Table 2")
+# Identify area codes from latest file
+Sub_Reg_Data_22 <- read_xlsx(Sub_Reg_Files[length(Sub_Reg_Files)], sheet = "Table 2")
+Area_Codes <- Sub_Reg_Data_22[3:nrow(Sub_Reg_Data_22), 1] #First column is area codes
+# Extract first 2 columns as empty text
+
+
 
 
 
