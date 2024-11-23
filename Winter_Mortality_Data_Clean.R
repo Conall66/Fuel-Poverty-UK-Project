67ff4a39fbee1fde7d33e78908f754ff163a3cc3
James Garside <james.garside@garsides.net>
2024-11-23 14:28:35 +0000

    Beginning winter mortality clean
    check check

---
+++ Winter_Mortality_Data_Clean.R
@@ -0,0 +1,19 @@
+# Data Collection and Cleaning
+
+
+# Packages ----------------------------------------------------------------
+
+# Read data
+library(rio)
+
+# To clean data
+library(tidyverse)
+library(lubridate)
+library(janitor) # cleans data sets (converts from single object to flattened table)
+
+# Plotting
+library(treemap)
+
+# Key Data sets Import -----------------------------------------------------
+
+WinterMoralityRate <-- ("")
