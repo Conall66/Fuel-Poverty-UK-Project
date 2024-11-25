7969d6603cd0cea39c8f8465b22e2fd7d881bf54
James Garside <james.garside@garsides.net>
2024-11-25 17:58:02 +0000

    kermitting suicide

--- mapTest.R
+++ mapTest.R
@@ -3,22 +3,22 @@ library(sf)
 library(ggplot2)
 library(dplyr)
 
-la_boundaries <- st_read("Local_Authority_Districts_May_2024_Boundaries_UK_BGC_-5850961694214429102")
+la_boundaries <- st_read("Local_Authority_Districts_December_2022_UK_BGC_V2_8602835163392188905")
 mortality_data <- read.csv("mortality_data.csv")
 
 # Select just 2011/2012 data
-mortality_data_2011 <- mortality_data %>%
-  select(`Area.code`, `Area.name`, Winter.mortality.index..2011.2012)
+mortality_data_2017 <- mortality_data %>%
+  select(`Area.code`, `Area.name`, Winter.mortality.index..2016.2017)
 
 # Then try joining with actual column names
 uk_boundaries_with_data <- la_boundaries %>%
-  left_join(mortality_data_2011, by = c("LAD24CD" = "Area.code"))
+  left_join(mortality_data_2017, by = c("LAD22CD" = "Area.code"))
 
 
 # Plot with standardized values for better color spread
 gg <- ggplot() + 
   geom_sf(data = uk_boundaries_with_data, 
-          aes(fill = scale(`Winter.mortality.index..2011.2012`)), 
+          aes(fill = scale(`Winter.mortality.index..2016.2017`)), 
           color = "white", 
           size = 0.25) +
   scale_fill_gradient2(low = "blue", mid = "red", high = "yellow", 
@@ -27,6 +27,5 @@ gg <- ggplot() +
 
 
 # To see the data structure:
-head(landarea_boundaries)
 ggsave("map.png", gg)
 
